827ad1449dddede3665a7eded88adbb8adf9de5c
Ori Nahum <orinahum@Oris-iMac.local>
2020-01-07 19:40:59 +0200

    add the newDayCounterDb

--- server/app.js
+++ server/app.js
@@ -4,15 +4,16 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 var bodyParser = require('body-parser')
 var nodemailer = require('nodemailer');
-
+ 
 const { setIntervalCurrency} = require('./currency/index')
 const { setIntervalWeather} = require('./weather/index')
 var {counter, getTotalCounter} = require('./counter/index')
-
+ 
 require('./db/mongoose')
 const Currency = require('./db/models/currency')
 const Weather = require('./db/models/weather')
-
+const insertNewDayCounterDb = require('./db/automation/counter')
+ 
 //setting of nodemailer
 var transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -21,33 +22,33 @@ var transporter = nodemailer.createTransport({
            pass: process.env.MYHOMY_EMAIL_PASS
        }
    });
-
+ 
 //setting the port
 const port = process.env.PORT || 3150;
 const dist = path.join(__dirname , '../dist')
-
+ 
 const app = express();
-
+ 
 app.use(express.static(dist));
 app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser());
-
+ 
 //start data get intervals
 setIntervalCurrency()
 setIntervalWeather()
-
+ 
 app.get ('', (req,res) => {
     res.render('index.html');
 })
-
-
-
+ 
+ 
+ 
 app.get ('/weather', async(req,res) => {
     //visitor counter - weather call when user open website
     counter();
-    //weather
-
+ 
+    //WEATHER
     //no cookies body
     if (!req.cookies.city) {
     return res.send( {
@@ -60,21 +61,21 @@ app.get ('/weather', async(req,res) => {
     } catch (e) {
       res.status(500).send()
   }
-    
+     
   })
-
+ 
     // console.log(req.cookies.city, req.cookies.lon, req.cookies.lat)
     // res.send( queryWeather(req.cookies.city, req.cookies.lon, req.cookies.lat))
-
-
+ 
+ 
 app.get ('/currency', async(req,res) => {
   //no query body
   if (!req.query.curnum) {
     return res.send( {
       'error': 'You must provide a currency number'
     }) }
-    
-    let fixNumber = req.query.curnum; 
+     
+    let fixNumber = req.query.curnum;
     //validate the number that is 2 digit
     if (req.query.curnum<10) {
         fixNumber = ('00'+req.query.curnum).slice(-2);
@@ -86,16 +87,16 @@ app.get ('/currency', async(req,res) => {
     } catch (e) {
       res.status(500).send()
   }
-    
+     
   })
-
-
+ 
+ 
 //post of contact send mail
 app.post('/contact', (req,res) => {
     console.log(req.body);
     const mailOptions = {
         from: 'homeyhomeyhomeyhomeyhomey@gmail.com', // sender address
-        to: 'ori@ezracpa.co.il', 
+        to: 'ori@ezracpa.co.il',
         subject: 'פנייה חדשה מאתר Homey', // Subject line
         html: `<p>name:&emsp;${req.body.name}<br>phone:&emsp;${req.body.phone}<br>email:&emsp;${req.body.email}<br>text:&emsp;${req.body.text}<br></p>`// plain text body
       };
@@ -106,20 +107,20 @@ app.post('/contact', (req,res) => {
           console.log(info);
      });
 });
-
+ 
 //send total visitor via mail
 app.get ('/counter', (req,res) => {
     sendCounter()
     return res.send(200, { message: 'ok' });
 })
-
-
+ 
+ 
 async function sendCounter () {
   const res = await getTotalCounter();
   const totalCounter = res[0].totalVisitors;
   const mailOptions = {
     from: 'homeyhomeyhomeyhomeyhomey@gmail.com', // sender address
-    to: 'ori@ezracpa.co.il', 
+    to: 'ori@ezracpa.co.il',
     subject: 'counter Homey', // Subject line
     html: `<p>total counter:&emsp;${totalCounter}</p>`// plain text body
   };
@@ -130,10 +131,17 @@ async function sendCounter () {
       console.log(info);
  });
 }
-//send on timer total visitor via mail
-const timersendCounter = setInterval(function () { sendCounter() },10*60*60*1000);//twenty four hours
-
-
+ 
+app.get('/newDayCounterDb', (req, res) => {
+    insertNewDayCounterDb();
+    return res.send(200, { message: 'ok' });
+})
+ 
+ 
+//send on timer total visitor via mail, not working well cuz the heroku server down every 30min
+// const timersendCounter = setInterval(function () { sendCounter() },10*60*60*1000);//twenty four hours
+ 
+ 
 app.listen (port, () => {
     console.log(`Server is up port ${port}`)
 })
